17ff16ce2b82f2585a16d830b6e1deaa9d11aaf7
real1player <55850728+real1player@users.noreply.github.com>
2020-07-29 12:52:23 +0300

    Add files via upload

---
+++ DataBaseConnection/src/main/java/main/Main.java
@@ -0,0 +1,25 @@
+package main;
+
+import dbService.DBException;
+import dbService.DBService;
+import dbService.dataSets.UsersDataSet;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        DBService dbService = new DBService();
+        dbService.printConnectInfo();
+        try {
+            long userId = dbService.addUser("tully");
+            System.out.println("Added user id: " + userId);
+
+            UsersDataSet dataSet = dbService.getUser(userId);
+            System.out.println("User data set: " + dataSet);
+
+            //dbService.cleanUp();
+        } catch (DBException e) {
+            e.printStackTrace();
+        }
+    }
+}
